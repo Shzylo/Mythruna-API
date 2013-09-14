5fb121abb02a72218ab8c47e6b67cb1418635ead
Shzylo <shzylo66@gmail.com>
2013-09-14 08:10:18 -0700

    Update on.java

--- examples/java/on.java
+++ examples/java/on.java
@@ -7,6 +7,7 @@ import mythruna.event.PlayerEvents;
 public class OnMethod {
 	public static void onPlayerJoined() { //Name this whatever you want, MUST BE PUBLIC AND STATIC!
 		// Adding the event
+		@Override
 		EventDispatcher.getInstance().addListener(PlayerEvents.playerJoined, new EventListener<PlayerEvent>() {
 			public void newEvent(EventType<PlayerEvent> type, PlayerEvent event) { //Registers the event
 				String playerName = event.getContext().getPlayerData().get("characterInfo.name");
